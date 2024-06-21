8a2f226389481d6542b8cc41bdf648b7d5c06660
Aryan <aryanmaurya879@gmail.com>
2024-06-21 20:18:31 +0530

    is array rotated and sorted LC1752

--- arrays/sorterarray/IsArraySorted.java
+++ arrays/sorterarray/IsArraySorted.java
@@ -3,19 +3,18 @@ package arrays.sorterarray;
 public class IsArraySorted {
 
   private static boolean isSorted(int[] arr) {
-    boolean sorted = true;
+    // boolean sorted = true;
     int n = arr.length;
     for (int i = 0; i < n - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        sorted = false;
-        break;
+        return false;
       }
     }
-    return sorted;
+    return true;
   }
 
   public static void main(String[] args) {
-    int arr[] = { 2, 3, 4, 52, 6, 7 };
+    int arr[] = { 2, 3, 4, 5, 6, 7 };
 
     System.out.println("array is sorted = " + isSorted(arr));
 
---
+++ arrays/sorterarray/IsRotatedArraySorted.java
@@ -0,0 +1,29 @@
+package arrays.sorterarray;
+
+public class IsRotatedArraySorted {
+
+  private static boolean isSorted(int[] arr) {
+    int countSortingFails = 0;
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+      // if count increases more than 1 then it fails
+      if (arr[i] > arr[i + 1]) {
+        countSortingFails++;
+      }
+    }
+    // checking the same for 1st and last element
+    if (arr[0] < arr[n - 1]) {
+      countSortingFails++;
+    }
+    System.out.println(countSortingFails);
+    return countSortingFails <= 1;
+  }
+
+
+  public static void main(String[] args) {
+    int arr[] = { 3, 4, 5, 1, 2 };
+
+    System.out.println("array is sorted = " + isSorted(arr));
+
+  }
+}
